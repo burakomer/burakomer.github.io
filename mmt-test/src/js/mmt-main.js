dbd814820d61be0b5ba7fdbb5dc46274297e045f
Mehmet Burak Ömer <eatyourgame@outlook.com>
2021-05-03 18:45:41 +0300

    Added MallMartTurk test site.

---
+++ mmt-test/src/js/mmt-main.js
@@ -0,0 +1,109 @@
+const ScreenSize = { xs: 1, sm: 2, md: 3, lg: 5, xl: 6 };
+Object.freeze(ScreenSize);
+
+jQuery(function () {
+  fetchNews();
+
+  responsiveUpdate();
+  $(window).on("resize", responsiveUpdate);
+  $(window).on("scroll", onScroll);
+});
+
+function responsiveUpdate() {
+  var deviceSize = ScreenSize[getBootstrapDeviceSize()];
+  
+  const navbarBrand = $(".navbar-brand");
+  if (deviceSize <= ScreenSize.sm) {
+    // Add padding to navbar-brand
+    navbarBrand.css("padding-left", "12px");
+  } else {
+    // Remove padding from navbar-brand
+    navbarBrand.css("padding-left", "0px");
+  }
+
+  const motto = $("h1#motto"); // Alter text shifting
+  const newsCards= $("#news .card-element"); // Change card column layout
+  if (deviceSize <= ScreenSize.md) {
+    motto.css("left", "5%");
+    newsCards.removeClass("col").addClass("col-sm-6");
+  } else {
+    motto.css("left", "10%");
+    newsCards.removeClass("col-sm-6").addClass("col");
+  }
+}
+
+function onScroll() {
+  var scroll = $(window).scrollTop();
+
+  if (scroll <= 10) {
+    $("div.go-top-button").animate({opacity: 0}, 0.2);
+  }
+  else {
+    $("div.go-top-button").animate({opacity: 1}, 0.2);
+  }
+}
+
+function fetchNews() {
+  const news = [
+    {
+      title: "Bright",
+      content: `The spectacle
+      before us was indeed sublime. The sky was cloudless and of a
+      deep dark blue. The spectacle
+      before us was indeed sublime.`,
+      link: "#",
+    },
+    {
+      title: "Shine",
+      content: `The sky was cloudless and of a
+      deep dark blue. The spectacle
+      before us was indeed sublime.`,
+      link: "#",
+    },
+    {
+      title: "Sun",
+      content: `The sky was cloudless and of a
+      deep dark blue. The spectacle
+      before us was indeed sublime. The sky was cloudless and of a
+      deep dark blue. The spectacle
+      before us was indeed sublime.`,
+      link: "#",
+    },
+    {
+      title: "Diamond",
+      content: `The sky was cloudless and of a
+      deep dark blue. The spectacle
+      before us was indeed sublime. `,
+      link: "#",
+    },
+    
+  ];
+
+  var newsHtml = "";
+
+  for (let i = 0; i < news.length; i++) {
+    const item = news[i];
+
+    newsHtml += `
+    <div class="col card-element g-4">
+      <div class="card-contents">
+        <ul>
+          <li>
+            <h1>${item.title}</h1>
+          </li>
+          <li>
+            <p>${item.content}</p>
+          </li>
+        </ul>
+        <div class="pt-5"><a href="${item.link}" class="pill-button"><span>Devamını oku...</span></a></div>
+      </div>
+    </div>
+    `;
+  }
+
+  $("#news #news-contents").append(newsHtml);
+}
+
+function getBootstrapDeviceSize() {
+  return $("#device-size-detector").find("div:visible").first().attr("id");
+}
